770f613680a2761a32c42e44d3b43f4e9639d47b
MaCeleste <mceleste.orona@gmail.com>
2022-10-07 22:33:30 +0100

    Final solution. Words are loaded from words.txt

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import random
 
-words = ['computer', 'hairspray', 'radiator']
+fileHandle = open('words.txt')
+words = fileHandle.read().split()
 
 wordToGuess = list(random.choice(words))
 puzzle = []
 
 for letter in wordToGuess:
-    puzzle.append('-')
-print(puzzle)
+    puzzle.append('_')
+print(' '.join(puzzle))
 
-numberOfGuesses = 8
+numberOfGuesses = 10
 
 while numberOfGuesses > 0:
-    if '-' in puzzle:
+    if '_' in puzzle:
         userGuess = input('Enter a letter:')
         if userGuess in puzzle:
             print('You already guessed this letter. Try a different one')
+            print(' '.join(puzzle))
             continue
         elif userGuess in wordToGuess:
             indices = [i for i, x in enumerate(wordToGuess) if x == userGuess]
-            print(indices)
             for index in indices:
                 puzzle[index] = userGuess
-            print(puzzle)
+            print(' '.join(puzzle))
         else:
             print('Wrong! Try again!')
             numberOfGuesses -= 1
+            print(' '.join(puzzle))
     else:
         print('You win!')
         break
